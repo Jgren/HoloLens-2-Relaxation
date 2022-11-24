c1614f5c557cbce71fabc5801005f1224722d44b
John Jarlgren <ojajee-1-local@B4702.ltuad.ltu.se>
2022-11-24 16:26:40 +0100

    Upload base files
    Initial commit

---
+++ Assets/SpinScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class SpinScript : MonoBehaviour
+{
+    public Transform zombieTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        zombieTransform.Rotate(new Vector3(0, 100 * Time.deltaTime, 0));
+    }
+}
